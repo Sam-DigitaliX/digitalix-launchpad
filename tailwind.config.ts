019d152f7b2a9ab0f4a40a1e1ca43282fcfe1783
Sam-DigitaliX <102250817+Sam-DigitaliX@users.noreply.github.com>
2025-12-27 03:39:17 +0100

    Update animation scroll indicator - tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -83,6 +83,11 @@ export default {
           to: { height: "0" },
         },
       },
+      /* 🔥 Scroll indicator premium animation */
+      float: {
+        "0%, 100%": { transform: "translate(-50%, 0)" },
+        "50%": { transform: "translate(-50%, 10px)" },
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
